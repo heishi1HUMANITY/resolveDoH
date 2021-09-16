b672e0a602f34a193ad691b623f25003f410e92c
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2021-09-16 11:42:58 +0900

    fix bench.ts

--- bench.ts
+++ bench.ts
@@ -5,7 +5,7 @@ import {
 } from "https://deno.land/std@0.103.0/testing/bench.ts";
 
 const domain = "www.example.com";
-const resolver = new URL("https://dns64.dns.google/dns-query");
+const resolver = new URL("https://dns.google/dns-query");
 
 bench({
   name: "resolveDoH speed",
